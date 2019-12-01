3535ea7c99a19ddf257737882cb5b7fb07f14895
chukeaa <54346014+chukeaa@users.noreply.github.com>
2019-12-01 20:23:26 +0800

    Add files via upload

--- DataProprocess.py
+++ DataProprocess.py
@@ -37,32 +37,52 @@ class DataProprocesser():
     # TODO:wash data-----start-----珂珂小仙女
     def wash_sanfen(self):
         # ready
+        data = self.data['sanfen']
+        patt = r'^(?P<engName>.*)/(?P<chiName>[ ]?[^ ]+)?(?P<Name>.+?\D+)(?P<size>\d+[g])?'
+        df_new = data['goods_name'].str.extract(patt,expand=True)
+        data = pd.concat([data,df_new], axis=1)
+#        data.to_csv(self.data_dir+'sanfen_data.csv',index =False)
+        data.to_csv(self.data_dir+'sanfen_test.csv',index =False)   
+        print(data)       
         return None
 
     def wash_zhexia(self):
-        # ready
-        return None
+        data = self.data['zhexia']
+        patt = r'^(?P<engName>.*)[\s](?P<Name>.+?\D+)(?P<size>\d+)?[g,ml]?'
+        df_new = data['goods_name'].str.extract(patt,expand=True)
+        data = pd.concat([data,df_new], axis=1)
+#        data.to_csv(self.data_dir+'sanfen_data.csv',index =False)
+        data.to_csv(self.data_dir+'zhexia_test.csv',index =False)   
+        print(data)  
 
-    def wash_fendiye(self):
-        return None
-
-    def wash_saihong(self):
-        return None
-
-    def wash_yanying(self):
-        # 先不弄
-        return None
-
-    def wash_kouhong(self):
-        return None
+    def wash_fendiye(self):      
+        data = self.data['fendiye']        
+        patt1 = r'^(?P<engName>.*\D+)[/](?P<chiName>[ ]?[^ ]+) (?P<Name1>[^ ]+)(?P<color>.*)?'
+        df_new = data['goods_name'].str.extract(patt1,expand=True)  
+        data = pd.concat([data,df_new], axis=1)
+        data.to_csv(self.data_dir+'fendiye_data.csv',index =False)                 
+        data = self.data['fendiye']         
+        patt2 = r'^(?P<Name>\D+)(?P<size>\d+)?[g,ml]+?'
+        df_new = data['Name1'].str.extract(patt2,expand=True)   
+        data = pd.concat([data,df_new], axis=1)
+#        data.to_csv(self.data_dir+'sanfen_data.csv',index =False)
+        data.to_csv(self.data_dir+'fendiye_test.csv',index =False)   
+        print(data)  
+#
+#    def wash_saihong(self):
+#        return None
+#
+#    def wash_yanying(self):
+#        # 先不弄
+#        return None
+#
+#    def wash_kouhong(self):
+#        return None
 
     # TODO:wash data-----end-----珂珂小仙女
 
-
-
-
 dp = DataProprocesser()
-dp.wash_meibi()
+#dp.wash_meibi()
 # brand = dp.data['xiangshui'][['engName','chiName']]
 # brand = brand.drop_duplicates()
 # brand.to_csv(dp.data_dir+'brand.csv',index = False)
